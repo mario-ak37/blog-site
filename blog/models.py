23881adc0e009402e65a9a40542ea0b9eb56e9f1
mario-ak37 <alfredkipkogei@proton.me>
2025-11-16 22:43:41 +0300

    quersets and managers and also building list and detail views

---
+++ blog/models.py
@@ -0,0 +1,46 @@
+from django.conf import settings
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
+
+# custom manager for published posts
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+
+    # default manager
+    objects = models.Manager()
+
+    # custom manager for published posts
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"id": self.pk})
---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
+
+# Create your views here.
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, "list.html", {"posts": posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request, "detail.html", {"post": post})
